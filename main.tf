4d9b2fe03e2a4fcaeb252067312cd4aa13ed463b
sammydev3564 <dadasammyajibola@gmail.com>
2022-12-23 17:50:34 +0100

    updated variables

--- main.tf
+++ main.tf
@@ -8,7 +8,9 @@ variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
-variable "my_ip"{}
+variable "my_ip" {}
+variable "instance_type" {}
+variable "public_key" {}
 
 #create vpc
 resource "aws_vpc" "myapp-vpc" {
@@ -28,28 +30,107 @@ resource "aws_subnet" "myapp-subnet-1" {
 }
 
 #create route table
-resource "aws_route_table" "myapp-route-table" {
-  vpc_id =aws_vpc.myapp-vpc.id
+#resource "aws_route_table" "myapp-route-table" {
+# vpc_id =aws_vpc.myapp-vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id  
-}
-tags = {
-    Name = "${var.env_prefix}-rtb"
-  }
-}
+#route {
+#  cidr_block = "0.0.0.0/0"
+#  gateway_id = aws_internet_gateway.myapp-igw.id  }
+#tags = {
+#  Name = "${var.env_prefix}-rtb"}
+#}
 
-  #create iternet gateway
-  resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id =aws_vpc.myapp-vpc.id
+#create internet gateway
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
   tags = {
     Name = "${var.env_prefix}-igw"
   }
 }
 #associate the route table
-resource "aws_route_table_association" "ass-rtb" {
-  subnet_id      = aws_subnet.myapp-subnet-1.id
-  route_table_id = aws_route_table.myapp-route-table.id
+#resource "aws_route_table_association" "ass-rtb" {
+# subnet_id      = aws_subnet.myapp-subnet-1.id
+# route_table_id = aws_route_table.myapp-route-table.id
+#}
+#c
+resource "aws_default_route_table" "myapp-route-table" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-rtb"
+  }
 }
-#create security group
+resource "aws_security_group" "myapp-sg" {
+  name        = "myapp-sg"
+  description = "allow ssh"
+  vpc_id      = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port        = "22"
+    to_port          = "22"
+    protocol         = "tcp"
+    cidr_blocks      = [var.my_ip]
+  }
+ingress {
+    from_port        = "8080"
+    to_port          = "8080"
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+  tags = {
+    Name = "${var.env_prefix}-sg"
+  }
+
+}
+data "aws_ami" "latest-amazon-ubuntu" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-*-20221201"]
+  }
+    filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "myapp-server" {
+  ami           = data.aws_ami.latest-amazon-ubuntu.id
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+  availability_zone = var.avail_zone
+  associate_public_ip_address = true
+   key_name = aws_key_pair.ssh-key.key_name
+tags = {
+    Name = "${var.env_prefix}-websever"}
+}
+
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-ubuntu.id
+}
+resource "aws_key_pair" "ssh-key" {
+  key_name   = "ssh-key"
+  public_key =var.public_key
+
+}
